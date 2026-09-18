00c6e140ba13c5535fa9d007920c3600f9deb3f7
aikohanasaki <future.skyline@gmail.com>
2026-09-18 12:29:37 -0600

    fix Topical Clip keywords

--- clipManager.js
+++ clipManager.js
@@ -2232,6 +2232,8 @@ export async function showTopicalClipPopup(options = {}) {
         void loadSelectedLorebook(event.target.value || '');
     });
     targetSelect?.addEventListener('change', () => {
+        const target = getSelectedTargetEntry();
+        if (keywordsInput) keywordsInput.value = getEntryKeys(target).join(', ');
         renderSourcePicker();
         renderTargetMetadataMessage();
         renderDiagnostics();
